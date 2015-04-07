d0bac3ef2ca6ba78726abc36fdd6f624fcc41663
Joao Barreiros <jgbbarreiros@gmail.com>
2015-04-07 17:22:00 +0100

    Implementação do contrutor inicialização LinkedList<No>

--- IASA/src/pee/Solucao.java
+++ IASA/src/pee/Solucao.java
@@ -1,7 +1,23 @@
 package pee;
 
-public class Solucao {
-	public void juntarInicio(No no) {
-		
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Solucao implements Iterable<No> {
+	
+	private LinkedList<No> percurso;
+	
+	public Solucao() {
+		this.percurso = new LinkedList<No>();
 	}
+	
+	@Override
+	public Iterator<No> iterator() {
+		return percurso.iterator();
+	}
+
+	public void juntarInicio(No no) {
+		percurso.addFirst(no);
+	}
+	
 }
